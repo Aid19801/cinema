2b73df6d2f51834cfa3998a73ad6a402b7dbc6a3
Bard <jba91@ostml0244857.home>
2017-08-23 20:59:21 +0100

    Login/Register HTML and php

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 var express = require('express');
 var app = express();
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "skyVideo",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query('select * from movies', (req, res) => {
+    console.log(res);
+  })
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+
+// var mysql = require('mysql');
+//
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "yourusername",
+//   password: "yourpassword"
+// });
+//
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+// hep1hOlkOg.s
 //Set the public folder
 app.use(express.static(__dirname + '/public'));
 
