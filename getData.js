3927a9b0f24caeac93a1682989dadd497dbfaa67
Adrian Thompson <adrian.thompson@sky.uk>
2017-08-24 19:55:49 +0100

    working json from sql

--- getData.js
+++ getData.js
@@ -1,40 +1,52 @@
-var mysql = require('mysql');
 
-exports.getMoviesData = function (req, res) {
-  let arr = [];
 
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "T$hompsin12",
-    database: "sys"
-  });
 
-  con.query('SELECT * FROM movies', function (error, results, fields) {
-    if (error) throw error;
+getMoviesData = function (req, res) {
 
-    results.map((each) => {
+let finalThing;
 
-      var newProgramObject = {
-        id: each.id,
-        title: each.title,
-        year: each.year,
-        length: each.length,
-        lead_actor: each.lead_actor,
-        synopsis: each.synopsis,
-        rating: each.rating,
-        certificate: each.certificate,
-        genre: each.genre
-      }
+// all of this a promise
+  const getTheFuckingData = () => {
 
-      arr.push(newProgramObject);
+    let arr = [];
+
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "T$hompsin12",
+      database: "sys"
+    });
+
+  	return new Promise((resolve) => {
+      con.query('SELECT * FROM movies', function (error, results, fields) {
+        if (error) throw error;
+
+        results.map((each) => {
+
+          var newProgramObject = {
+            id: each.id,
+            title: each.title,
+            year: each.year,
+            length: each.length,
+            lead_actor: each.lead_actor,
+            synopsis: each.synopsis,
+            rating: each.rating,
+            certificate: each.certificate,
+            genre: each.genre
+          }
+
+          arr.push(newProgramObject);
+
+        })
+        resolve(arr);
+      });
     })
-    return arr;
-  });
+  }
 
-setTimeout(function () {
-  console.log('bbuibuibuibiu');
-}, 2000);
+  getTheFuckingData().then((result) => {
+    finalThing = result;
+    return finalThing;
+  })
 
-return arr;
+  return finalThing;
 }
