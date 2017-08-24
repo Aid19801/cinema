b615126db44c75f34792020c7b030ac2dbd2061c
Adrian Thompson <adrian.thompson@sky.uk>
2017-08-24 08:27:55 +0100

    moduleing

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 var express = require('express');
 var app = express();
-
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "T$hompsin12",
-  database: "sys"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  con.query('SELECT * FROM movies', (req, res) => {
-    console.log('res: ----> ', res);
-  })
-});
+// var arr = [];
+//
+// // connecting to mySQL
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "T$hompsin12",
+//   database: "sys"
+// });
+//
+// // taking everything from the movies table
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query('SELECT * FROM movies', (req, res) => {
+//     // console.log(res);
+//   })
+// });
+//
+// arr.push('i am a pushed arr element');
+//
+// module.exports = arr;
+//
+// console.log('arr: ', arr[0]);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
